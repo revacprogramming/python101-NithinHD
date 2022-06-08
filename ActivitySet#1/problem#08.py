b6ce7f59b776c487a1bbf699066d9f6cf9427737
Nithin HD <96277732+NithinHD@users.noreply.github.com>
2022-06-08 03:46:52 +0000

    new

---
+++ ActivitySet#1/problem#08.py
@@ -0,0 +1,14 @@
+fh=open(input('Enter the file name:'))
+count=0
+total=0
+a=[]
+for line in fh:
+    if line.startswith("X-DSPAM-Confidence:"):
+        n=line.find(':')
+        b=float(line[n+1:])
+        a.append(b)
+for i in a:
+    count=count+1
+    total=total+i
+avg=total/count
+print('average spam confidence =',avg)
