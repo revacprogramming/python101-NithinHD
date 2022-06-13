a497dd5336cc9e6fd49e427bf92a0930c926f296
Nithin HD <96277732+NithinHD@users.noreply.github.com>
2022-06-13 10:04:52 +0000

    redo

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,8 +1,9 @@
-h = float(input("Enter hours? "))
-r=float(input('Enter the Rate per Hour:'))
-if h<=40:
-    fee=h*r
-    print('the fee is',fee)
-elif h>40:
-    fee=(40*r)+((h-40)*r*1.5)
-    print('the fee is',fee)
+hrs=float(input('Enter the number of hours:'))
+rate=float(input('Enter the rate per hour'))
+if hrs<40:
+  fee=hrs*rate
+  print('The Fee is ',fee)
+if not hrs<40:
+  h=hrs-40
+  fee=(40*rate)+(h*1.5*rate)
+  print('The Fee is ',fee)
